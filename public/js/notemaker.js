81832bb4bfac322af2c66f7a1b0f12ee36c95079
Malinna Leach <malinnab@yahoo.com>
2016-11-07 16:40:43 +0000

    can print text input

--- public/js/notemaker.js
+++ public/js/notemaker.js
@@ -1,7 +1,6 @@
-var form = document.createElement('form');
-var createNote = function(){
-  console.log("Hello")
-};
-form.setAttribute('action', function(){createNote()});
-
 console.log(document);
+
+
+var createNote = function(){
+  console.log(document.getElementById('note').value);
+};
