243cb98f516e5edc98a32ea0b027f50ab14453d2
Malinna Leach <malinnab@yahoo.com>
2016-11-08 12:54:42 +0000

    adds full note

--- public/js/notemaker.js
+++ public/js/notemaker.js
@@ -1,10 +1,25 @@
 var noteLibrary = [];
+var counter = 1;
 
 var createNote = function(){
   var note = document.getElementById('note').value;
   noteLibrary.push(note);
   var list = document.createElement('li');
-  var txt = document.createTextNode(note.substring(0,20) + "...");
-  list.appendChild(txt);
+  var btn = document.createElement('input');
+  btn.setAttribute('type', 'button');
+  btn.setAttribute('id', counter);
+  btn.setAttribute('onclick', 'showNote(this.id)');
+  btn.setAttribute('value', note.substring(0,20) + "...")
+  list.appendChild(btn);
   document.getElementById('listed_notes').appendChild(list);
+  var para = document.createElement('p');
+  para.setAttribute('id', "full_" + counter);
+  para.innerHTML = note;
+  document.getElementById('full_notes').appendChild(para);
+  counter++;
+  console.log(document)
+};
+
+var showNote() = function(id) {
+
 };
